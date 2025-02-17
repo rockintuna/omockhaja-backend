3184f254baef97499cbaa43d0c10ed7582754fe0
rockintuna <rockintuna@naver.com>
2025-02-17 21:15:34 +0900

    first commit

---
+++ src/main/java/me/rockintuna/omockhaja/OmockhajaApplication.java
@@ -0,0 +1,13 @@
+package me.rockintuna.omockhaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmockhajaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmockhajaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rockintuna/omockhaja/OmockhajaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.omockhaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmockhajaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
